7a4fb7bac57e89a3536080b117334d45ce1affad
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-07-03 06:27:41 -0400

    Added fontawesome, google fonts, styling

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Portfolio
     # Generate *.js files instead of *.coffee files
     config.generators.javascript_engine = :js
 
+    # Pre-Compile fonts
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
